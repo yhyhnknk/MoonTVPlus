8f584c13ff1685cf13b9026f588a89d3b9f219d9
yhyhnk <erdongba@gmail.com>
2026-09-23 12:01:57 +0800

    Update route.ts

--- src/app/api/admin/user/route.ts
+++ src/app/api/admin/user/route.ts
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic';
-
 /* eslint-disable @typescript-eslint/no-explicit-any,no-console,@typescript-eslint/no-non-null-assertion */
 
 import { NextRequest, NextResponse } from 'next/server';
